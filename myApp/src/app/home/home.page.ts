5c033796956cc922af603f3ce098343bf8260e4d
ciceribeiroo <ciceribeiroo@gmail.com>
2021-03-01 20:21:04 -0300

    Problema com componente ionic

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+})
+@NgModule({
+  imports: [
+    IonicModule
+  ]
 })
 export class HomePage {
 
