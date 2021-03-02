2203833d76108355f87bde0b27d8928f0bf57394
ciceribeiroo <alicinha2r@gmail.com>
2021-03-02 14:31:06 -0300

    Adicionando categorias, mudando cor e letra

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { IonicModule, ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,14 +18,44 @@ export class HomePage {
   weight: number;
   height: number;
 
-  constructor(private tosastController: ToastController) {}
+  constructor(private tosastController: ToastController, public alertController: AlertController) {}
 
+    async presentAlert(msg: string) {
+      const alert = await this.alertController.create({
+        header: 'Atenção!',
+        subHeader: `Seu IMC foi classificado como ${msg}`,
+        message: 'Para mais informações, procure um profissional de saúde',
+        buttons: ['Entendi'],
+      });
+    
+      await alert.present();
+      let result = await alert.onDidDismiss();
+      console.log(result);
+    }
     isFormValid(){
       return (this.height && this.weight && this.height>0 && this.weight >0);
     }
     onCalculate(){
       const imc = this.weight / (this.height*this.height)
-      this.showMessage(`IMC = ${imc.toFixed(2)}`)
+      const msg = this.category(imc);
+      if(msg == 'Obesidade' || msg == 'Obesidade Grave'){
+        this.presentAlert(msg);
+      }
+      this.showMessage(`Seu IMC é ${imc.toFixed(3)} que corresponde a categoria ${msg}`)
+    }
+    category(imc: number){
+      if(imc < 18.5){
+          return 'Magreza'
+      }
+      else if(imc >= 18.5 && imc< 24.9){
+          return 'Normal'
+      }else if(imc >= 25 && imc < 29.9){
+          return 'Sobrepeso'
+      }else if(imc >= 30 && imc < 39.9){
+          return 'Obesidade'
+      }else{
+          return 'Obesidade Grave'
+      }
     }
 
     async showMessage(msg: string){
